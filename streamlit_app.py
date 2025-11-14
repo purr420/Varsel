69404012c71b86ac9cc23d2c26ef99bb0dff785d
pete <107641328+purr420@users.noreply.github.com>
2025-11-14 23:11:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,8 +68,8 @@ I dag 14. nov – Første lys 07:52 – Sol opp 08:15 – Sol ned 16:32 – Sist
 <thead>
 <tr>
     <th>Tid</th>
-    <th>Dønning H</th><th>Dønning P</th><th>Dønning R</th>
-    <th>Vindbølger H</th><th>Vindbølger P</th><th>Vindbølger R</th>
+    <th>Dønning m</th><th>Dønning s</th><th>Dønning retning</th>
+    <th>Vindbølger m</th><th>Vindbølger s</th><th>Vindbølger retning</th>
     <th>P.dom.</th>
     <th>yr Vind (m/s)</th><th>yr Retning</th>
     <th>dmi Vind (m/s)</th><th>dmi Retning</th>
@@ -78,9 +78,9 @@ I dag 14. nov – Første lys 07:52 – Sol opp 08:15 – Sol ned 16:32 – Sist
 </thead>
 
 <tbody>
-<tr><td>16</td><td>1,2</td><td>9,6</td><td>VSV</td><td>1,2</td><td>6</td><td>NNV</td><td>4,7</td><td>4</td><td>NNV</td><td>4</td><td>V</td><td>10°C</td><td>12°C</td><td>0 %</td><td></td></tr>
-<tr><td>17</td><td>1,3</td><td>9,3</td><td>VSV</td><td>1,0</td><td>5</td><td>NNV</td><td>4,8</td><td>5</td><td>NNV</td><td>5</td><td>V</td><td>10°C</td><td>12°C</td><td>10 %</td><td></td></tr>
-<tr><td>18</td><td>1,2</td><td>9,2</td><td>SV</td><td>0,9</td><td>5</td><td>NV</td><td>6,10</td><td>11</td><td>NV</td><td>11</td><td>V</td><td>10°C</td><td>12°C</td><td>40 %</td><td></td></tr>
+<tr><td>16</td><td>1,2 m</td><td>9,6 s</td><td>VSV</td><td>1,2 m</td><td>6 s</td><td>NNV</td><td>9,8 s</td><td>4(7)</td><td>NNV</td><td>4(7)</td><td>V</td><td>10°C</td><td>12°C</td><td>0 %</td><td></td></tr>
+<tr><td>17</td><td>1,3 m</td><td>9,3 s</td><td>VSV</td><td>1,0 m</td><td>5 s</td><td>NNV</td><td>9,7 s</td><td>4(8)</td><td>NV</td><td>5(10)</td><td>NNV</td><td>10°C</td><td>12°C</td><td>10 %</td><td></td></tr>
+<tr><td>18</td><td>1,2 m</td><td>9,2 s</td><td>SV</td><td>0,9 m</td><td>5 s</td><td>NV</td><td>9,6 s</td><td>6(10)</td><td>V</td><td>11(13)</td><td>NV</td><td>10°C</td><td>12°C</td><td>40 %</td><td></td></tr>
 </tbody>
 </table>
 </div>
@@ -95,8 +95,8 @@ I morgen 15. nov – Første lys 07:55 – Sol opp 08:19 – Sol ned 16:29 – S
 <thead>
 <tr>
     <th>Tid</th>
-    <th>Dønning H</th><th>Dønning P</th><th>Dønning R</th>
-    <th>Vindbølger H</th><th>Vindbølger P</th><th>Vindbølger R</th>
+    <th>Dønning m</th><th>Dønning s</th><th>Dønning retning</th>
+    <th>Vindbølger m</th><th>Vindbølger s</th><th>Vindbølger retning</th>
     <th>P.dom.</th>
     <th>yr Vind (m/s)</th><th>yr Retning</th>
     <th>dmi Vind (m/s)</th><th>dmi Retning</th>
@@ -105,13 +105,13 @@ I morgen 15. nov – Første lys 07:55 – Sol opp 08:19 – Sol ned 16:29 – S
 </thead>
 
 <tbody>
-<tr><td>06</td><td>1,2</td><td>9,6</td><td>VSV</td><td>1,2</td><td>6</td><td>NNV</td><td>4,7</td><td>4</td><td>NNV</td><td>4</td><td>V</td><td>7°C</td><td>12°C</td><td>100 %</td><td>0,9 mm</td></tr>
-<tr><td>07</td><td>1,3</td><td>9,3</td><td>VSV</td><td>1,0</td><td>5</td><td>NNV</td><td>4,8</td><td>5</td><td>NNV</td><td>5</td><td>V</td><td>7°C</td><td>12°C</td><td>100 %</td><td>1,1 mm</td></tr>
-<tr><td>08</td><td>1,2</td><td>9,2</td><td>SV</td><td>0,9</td><td>5</td><td>NV</td><td>4,7</td><td>4</td><td>NNV</td><td>4</td><td>V</td><td>8°C</td><td>12°C</td><td>100 %</td><td>0,3 mm</td></tr>
-<tr><td>09</td><td>1,2</td><td>9,6</td><td>VSV</td><td>1,2</td><td>6</td><td>NNV</td><td>4,8</td><td>5</td><td>NNV</td><td>5</td><td>V</td><td>9°C</td><td>12°C</td><td>90 %</td><td></td></tr>
-<tr><td>10</td><td>1,2</td><td>9,6</td><td>VSV</td><td>1,2</td><td>6</td><td>NNV</td><td>4,7</td><td>4</td><td>NNV</td><td>4</td><td>V</td><td>10°C</td><td>12°C</td><td>70 %</td><td></td></tr>
-<tr><td>11</td><td>1,3</td><td>9,3</td><td>VSV</td><td>1,0</td><td>5</td><td>NNV</td><td>4,8</td><td>5</td><td>NNV</td><td>5</td><td>V</td><td>11°C</td><td>12°C</td><td>100 %</td><td>0,5 mm</td></tr>
-<tr><td>12</td><td>1,2</td><td>9,2</td><td>SV</td><td>0,9</td><td>5</td><td>NV</td><td>6,10</td><td>11</td><td>NV</td><td>11</td><td>V</td><td>11°C</td><td>12°C</td><td>100 %</td><td>0,9 mm</td></tr>
+<tr><td>06</td><td>1,2 m</td><td>9,6 s</td><td>VSV</td><td>1,2 m</td><td>6 s</td><td>NNV</td><td>9,8 s</td><td>4(7)</td><td>NNV</td><td>4(7)</td><td>V</td><td>7°C</td><td>12°C</td><td>100 %</td><td>0,9 mm</td></tr>
+<tr><td>07</td><td>1,3 m</td><td>9,3 s</td><td>VSV</td><td>1,0 m</td><td>5 s</td><td>NNV</td><td>9,7 s</td><td>4(8)</td><td>NV</td><td>5(10)</td><td>NNV</td><td>7°C</td><td>12°C</td><td>100 %</td><td>1,1 mm</td></tr>
+<tr><td>08</td><td>1,2 m</td><td>9,2 s</td><td>SV</td><td>0,9 m</td><td>5 s</td><td>NV</td><td>9,6 s</td><td>4(7)</td><td>NNV</td><td>4(7)</td><td>V</td><td>8°C</td><td>12°C</td><td>100 %</td><td>0,3 mm</td></tr>
+<tr><td>09</td><td>1,2 m</td><td>9,6 s</td><td>VSV</td><td>1,2 m</td><td>6 s</td><td>NNV</td><td>9,8 s</td><td>4(8)</td><td>NV</td><td>5(10)</td><td>NNV</td><td>9°C</td><td>12°C</td><td>90 %</td><td></td></tr>
+<tr><td>10</td><td>1,2 m</td><td>9,6 s</td><td>VSV</td><td>1,2 m</td><td>6 s</td><td>NNV</td><td>9,8 s</td><td>4(7)</td><td>NNV</td><td>4(7)</td><td>V</td><td>10°C</td><td>12°C</td><td>70 %</td><td></td></tr>
+<tr><td>11</td><td>1,3 m</td><td>9,3 s</td><td>VSV</td><td>1,0 m</td><td>5 s</td><td>NNV</td><td>9,7 s</td><td>4(8)</td><td>NV</td><td>5(10)</td><td>NNV</td><td>11°C</td><td>12°C</td><td>100 %</td><td>0,5 mm</td></tr>
+<tr><td>12</td><td>1,2 m</td><td>9,2 s</td><td>SV</td><td>0,9 m</td><td>5 s</td><td>NV</td><td>9,6 s</td><td>6(10)</td><td>V</td><td>11(13)</td><td>NV</td><td>11°C</td><td>12°C</td><td>100 %</td><td>0,9 mm</td></tr>
 </tbody>
 </table>
 </div>
